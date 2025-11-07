bb32cbd897063f85d7cb6d6af8d47aca96e68134
Cédric BERNARD <147041546+SirDec73@users.noreply.github.com>
2025-11-07 15:38:09 +0100

    a

--- Assets/Components/ProceduralGeneration/3_Noise/TerrainNoise.cs
+++ Assets/Components/ProceduralGeneration/3_Noise/TerrainNoise.cs
@@ -31,9 +31,8 @@ public class TerrainNoise : Noise
 
     TerrainData GenerateTerrain(TerrainData td)
     {
-        td.heightmapResolution = Grid.Width;
+        td.size = new Vector3(Grid.Width, HEIGHT, Grid.Lenght);
 
-        td.size = new Vector3 (Grid.Width, HEIGHT, Grid.Lenght);
 
         td.SetHeights(0, 0, GenerateHeight());
         return td;
@@ -44,24 +43,24 @@ public class TerrainNoise : Noise
     {
         float[,] noiseData = new float[Grid.Width,Grid.Lenght];
 
-        for (int x = 0; x < Grid.Lenght; x++)
+        for (int x = 0; x < Grid.Width; x++)
         {
-            for (int y = 0; y < Grid.Width; y++) 
+            for (int y = 0; y < Grid.Lenght; y++) 
             {
-                float noiseCoordinate = GetNoiseData(x, y);
-                noiseData[x,y] = CalculateHeight(x, y, noiseCoordinate);
+                noiseData[x,y] = GetNoiseData(x, y);
+                //noiseData[x,y] = CalculateHeight(x, y, noiseCoordinate);
             }
         }
 
         return noiseData;
     }
 
-    float CalculateHeight(int x, int y, float scale)
-    {
-        float xCoord = (float)x / Grid.Width * 100 * scale;
-        float yCoord = (float)y / Grid.Lenght * 100 * scale;
-
-        return Mathf.PerlinNoise(xCoord, yCoord);
-
-    }
+    //float CalculateHeight(int x, int y, float scale)
+    //{
+    //    float xCoord = (float)x / Grid.Width * 100 * scale;
+    //    float yCoord = (float)y / Grid.Lenght * 100 * scale;
+    //
+    //    return Mathf.PerlinNoise(xCoord, yCoord);
+    //
+    //}
 }
